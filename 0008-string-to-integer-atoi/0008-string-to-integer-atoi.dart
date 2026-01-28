2313a818f6729bd1a9d49601756c432132c950dc
Husna Noushad <husnaanoushad@gmail.com>
2026-01-28 09:35:13 +0530

    Time: 1 ms (91.11%), Space: 149.9 MB (48.89%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.dart
@@ -0,0 +1,33 @@
+class Solution {
+  int myAtoi(String s) {
+    int i = 0;
+    int n = s.length;
+    int sign = 1;
+    int result = 0;
+
+    const int INT_MAX = 2147483647;
+    const int INT_MIN = -2147483648;
+
+    while (i < n && s[i] == ' ') {
+      i++;
+    }
+
+    if (i < n && (s[i] == '-' || s[i] == '+')) {
+      sign = (s[i] == '-') ? -1 : 1;
+      i++;
+    }
+
+    while (i < n && s.codeUnitAt(i) >= 48 && s.codeUnitAt(i) <= 57) {
+      int digit = s.codeUnitAt(i) - 48;
+
+      if (result > (INT_MAX - digit) ~/ 10) {
+        return sign == 1 ? INT_MAX : INT_MIN;
+      }
+
+      result = result * 10 + digit;
+      i++;
+    }
+
+    return result * sign;
+  }
+}

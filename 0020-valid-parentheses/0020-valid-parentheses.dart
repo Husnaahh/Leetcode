1a3c45903bc0b750ffa747b21114db4fc82664a1
Husna Noushad <husnaanoushad@gmail.com>
2026-02-06 21:00:39 +0530

    Time: 8 ms (31.12%), Space: 149.4 MB (49.65%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.dart
@@ -0,0 +1,25 @@
+class Solution {
+  bool isValid(String s) {
+    List<String> stack = [];
+
+    Map<String, String> pairs = {
+      ')': '(',
+      ']': '[',
+      '}': '{',
+    };
+
+    for (int i = 0; i < s.length; i++) {
+      String ch = s[i];
+
+      if (pairs.containsValue(ch)) {
+        stack.add(ch); 
+      } else {
+        if (stack.isEmpty || stack.removeLast() != pairs[ch]) {
+          return false;
+        }
+      }
+    }
+
+    return stack.isEmpty;
+  }
+}

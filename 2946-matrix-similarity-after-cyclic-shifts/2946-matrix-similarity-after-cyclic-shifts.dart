140f1204555bf4b1abea3d477db4201736d26bfa
Husna Noushad <husnaanoushad@gmail.com>
2026-03-27 19:32:47 +0530

    Time: 1 ms (100%), Space: 148.5 MB (100%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.dart
@@ -0,0 +1,27 @@
+class Solution {
+  bool areSimilar(List<List<int>> mat, int k) {
+    int m = mat.length;
+    int n = mat[0].length;
+
+    int shift = k % n;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+
+        int newCol;
+
+        if (i % 2 == 0) {
+          newCol = (j + shift) % n;
+        } else {
+          newCol = (j - shift % n + n) % n;
+        }
+
+        if (mat[i][j] != mat[i][newCol]) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}

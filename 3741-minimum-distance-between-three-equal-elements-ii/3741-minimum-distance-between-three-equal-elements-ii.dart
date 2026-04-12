c12eca4d4051f02ab75962d74864ec9d1d65207a
Husna Noushad <husnaanoushad@gmail.com>
2026-04-12 19:58:42 +0530

    Time: 236 ms (100%), Space: 334.9 MB (100%) - LeetHub

---
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.dart
@@ -0,0 +1,23 @@
+class Solution {
+  int minimumDistance(List<int> nums) {
+    Map<int, List<int>> map = {};
+
+    for (int i = 0; i < nums.length; i++) {
+      map.putIfAbsent(nums[i], () => []);
+      map[nums[i]]!.add(i);
+    }
+
+    int ans = 1 << 60; 
+
+    for (var list in map.values) {
+      if (list.length < 3) continue;
+
+      for (int i = 0; i + 2 < list.length; i++) {
+        int dist = 2 * (list[i + 2] - list[i]);
+        if (dist < ans) ans = dist;
+      }
+    }
+
+    return ans == (1 << 60) ? -1 : ans;
+  }
+}
